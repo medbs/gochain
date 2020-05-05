97cef700ce8f6a4cb4ca67caffdd4a9adf2e06b4
MedBs <med.boulaares@gmail.com>
2020-05-06 00:26:26 +0100

    add generate block unit test

---
+++ core/block_operations_test.go
@@ -0,0 +1,28 @@
+package core
+
+import "testing"
+
+func TestGenerateBlock(t *testing.T) {
+
+	b1 := Block{
+		Index:     1,
+		Timestamp: "stamp",
+		BPM:       "bpm",
+		Hash:      "hash",
+		PrevHash:  "prevhash",
+	}
+
+	generatedBlock := GenerateBlock(b1, "bpm2")
+
+	if generatedBlock.PrevHash != b1.Hash {
+		t.Errorf("the generated block previous has is not equal to the current block hash")
+	}
+
+	if generatedBlock.Index != b1.Index+1 {
+		t.Errorf("the generated block index should be previous index + 1 ")
+	}
+
+	if generatedBlock.BPM != "bmp2" {
+		t.Errorf("BPM value not assigned correctly")
+	}
+}
