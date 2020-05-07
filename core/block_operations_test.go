026819a3656ee4911c1ed0d2033023022b442be8
MedBs <med.boulaares@gmail.com>
2020-05-08 00:10:38 +0100

    add unit test for isBlockValid

--- core/block_operations_test.go
+++ core/block_operations_test.go
@@ -1,6 +1,8 @@
 package core
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGenerateBlock(t *testing.T) {
 
@@ -27,6 +29,42 @@ func TestGenerateBlock(t *testing.T) {
 	}
 }
 
-func TestCalculateHash(t *testing.T){
+func TestCalculateHash(t *testing.T) {
+	b1 := Block{
+		Index:     1,
+		Timestamp: "stamp",
+		BPM:       "bpm",
+		Hash:      "hash",
+		PrevHash:  "prevhash",
+	}
+	//the know value of block hash
+	correctHash := "41064f80255aee51e0e954f42ec238099eb636fbf11ffb929ecdb809d5d0dc6f"
+	blockHash := CalculateHash(b1)
 
+	if blockHash != correctHash {
+		t.Errorf("returned value is not correct")
+	}
+
+}
+
+func TestIsBlockValid(t *testing.T) {
+	b1 := Block{
+		Index:     1,
+		Timestamp: "stamp",
+		BPM:       "bpm",
+		Hash:      "41064f80255aee51e0e954f42ec238099eb636fbf11ffb929ecdb809d5d0dc6f",
+		PrevHash:  "prevhash",
+	}
+
+	b2 := Block{
+		Index:     2,
+		Timestamp: "stamp",
+		BPM:       "bpm",
+		Hash:      "4813e8a674e20be91754a8a92b8e01f35767fbdd13dc988fe0260943d1f17e1a",
+		PrevHash:  "41064f80255aee51e0e954f42ec238099eb636fbf11ffb929ecdb809d5d0dc6f",
+	}
+
+	if !IsBlockValid(b2, b1) {
+		t.Errorf("next block cannot be validated")
+	}
 }
